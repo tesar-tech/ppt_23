5c1139d2da841cf8d661077330dec9d2de69c0c0
tesar-tech <jan@tesar.tech>
2023-02-21 16:51:43 +0100

    git ignore cv1

--- 01/HadaniCiselConsole/Program.cs
+++ 01/HadaniCiselConsole/Program.cs
@@ -1,45 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Console.WriteLine("Ahoj vítej ve hře");
 
 
-int mysleneCislo = VymysliSiCislo();
+int mysleneCislo = NahodneCislo();
 
 while (true)
 {
-    Console.WriteLine("Hádej číslo:");
+    Console.Write("Hadej cislo:");
     string? vstup = Console.ReadLine();
-    if (vstup == null) return;
 
     if (vstup == "konec")
         break;
 
     if (!int.TryParse(vstup, out int hadaneCislo))
-    { 
-        Console.WriteLine("Není to číslo, piš číslo!!");
+    {
+        Console.WriteLine("Napiš číslo");
         continue;
     }
 
+    string vetsiNeboMensi = hadaneCislo < mysleneCislo ? "menší" : "větší";
+
+    Console.WriteLine($" Tvoje cislo je {vetsiNeboMensi} nez myslene");
+
     if (hadaneCislo == mysleneCislo)
     {
-        Console.WriteLine($"Ano, hadane cislo je {hadaneCislo}");
-        Console.WriteLine($"Hrát znovu? (A/N)");
-        string? hratZnovu = Console.ReadLine();
-        if (hratZnovu != null && hratZnovu.ToLower() == "a")
+        Console.WriteLine($"Uhadl jsi cislo {hadaneCislo}");
+        Console.WriteLine("Chces znovu (A/N)?");
+
+
+        string? aNn = Console.ReadLine();
+        
+        if (aNn?.ToLower() == "a")
         {
             Console.Clear();
-            mysleneCislo = VymysliSiCislo();
+            mysleneCislo = NahodneCislo();
         }
         else
             break;
+
     }
-    string mensiNeboVetsi = hadaneCislo < mysleneCislo ? "MENŠÍ" : "VĚTŠÍ";
-    Console.WriteLine($"Tvoje cislo je {mensiNeboVetsi} než myšlené číslo. Zkus to znovu");
 
 }
 
-Console.WriteLine("Díky za hru");
-Console.ReadLine();
 
-int VymysliSiCislo() => Random.Shared.Next(99) + 1;
+
+int NahodneCislo() => Random.Shared.Next(1, 100);
+
+
+
+
 
 
 
